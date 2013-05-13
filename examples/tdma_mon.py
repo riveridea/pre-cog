c6ef3bb7f1f0a60cad2e0b119daa8bdf389ede9c
alex zhang <cingluar.alex@gmail.com>
2013-05-12 23:17:12 -0500

    set the lead time to 5ms

--- examples/tdma_mon.py
+++ examples/tdma_mon.py
@@ -301,7 +301,7 @@ class my_top_block(gr.top_block):
                                                     0.030,#options.slot_interval,
                                                     0.000,#options.guard_interval,
                                                     1,    #number_of_slots,#options.number_of_slots,
-                                                    0.001,#options.lead_limit,
+                                                    0.005,#options.lead_limit,
                                                     self.link_rate))
     
     def setup_packet_framers(self):
