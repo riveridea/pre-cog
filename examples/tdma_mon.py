2d594d65fd725fdbdd9e75c10c71157a2ec26f3a
alex zhang <cingluar.alex@gmail.com>
2013-05-12 23:40:15 -0500

    debug

--- examples/tdma_mon.py
+++ examples/tdma_mon.py
@@ -298,8 +298,8 @@ class my_top_block(gr.top_block):
             initial_slot = NODES_PC*self._node_id + i
             number_of_slots = NETWORK_SIZE
             self.tdmaegns.append(precog.tdma_engine(initial_slot,
-                                                    0.030,#options.slot_interval,
-                                                    0.005,#options.guard_interval,
+                                                    0.050,#options.slot_interval,
+                                                    0.010,#options.guard_interval,
                                                     1,    #number_of_slots,#options.number_of_slots,
                                                     0.005,#options.lead_limit,
                                                     self.link_rate))
