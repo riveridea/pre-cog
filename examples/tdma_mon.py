ce223186ad207427e15868c03de40f406c83b941
alex zhang <cingluar.alex@gmail.com>
2013-05-13 21:55:58 -0500

    testing multi

--- examples/tdma_mon.py
+++ examples/tdma_mon.py
@@ -347,7 +347,7 @@ class my_top_block(gr.top_block):
         print 'setup_multiply_consts'
         self.mlts = []
         for i in range(self.n_devices):
-            self.mlts.append(gr.multiply_const_vcc((0.5, )))
+            self.mlts.append(gr.multiply_const_vcc((0.55, )))
     
     def setup_burst_gates(self):
         print 'setup_burst_gates'
