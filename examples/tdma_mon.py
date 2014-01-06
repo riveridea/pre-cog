3c42b8640c4e907bd032d689f2d6b22b89ceeb54
Your Name <you@example.com>
2014-01-06 12:36:29 -0600

    set the init slot to 1 to test if the blank can be solved

--- examples/tdma_mon.py
+++ examples/tdma_mon.py
@@ -375,11 +375,11 @@ class my_top_block(gr.top_block):
                                                         self.randbinfile))
             else:
                 # if tx_only == True, all the transmitter will transmitt simuletaneously and continuously.
-                self.tdmaegns.append(precog.tdma_engine(0,
+                self.tdmaegns.append(precog.tdma_engine(1,
                                                         0.100,#options.slot_interval,
                                                         0.0,#options.guard_interval,
                                                         1,    #number_of_slots,#options.number_of_slots,
-                                                        0.005,#options.lead_limit,
+                                                        0.000,#options.lead_limit,
                                                         self.link_rate,
                                                         self.addrs[i],
                                                         self.randbinfile))                
