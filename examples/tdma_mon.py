f101652a1c0b35550d97b25d4f019acbeda89625
Your Name <you@example.com>
2014-01-27 00:28:17 -0600

    increase the gurad interval to test if the LLL problem can be solved or not

--- examples/tdma_mon.py
+++ examples/tdma_mon.py
@@ -377,9 +377,9 @@ class my_top_block(gr.top_block):
                 number_of_slots = NETWORK_SIZE
                 self.tdmaegns.append(precog.tdma_engine(initial_slot,
                                                         0.100,#options.slot_interval,
-                                                        0.010,#options.guard_interval,
+                                                        0.040,#options.guard_interval,
                                                         70,    #number_of_slots,#options.number_of_slots,
-                                                        0.005,#options.lead_limit,
+                                                        0.000,#options.lead_limit,
                                                         self.link_rate,
                                                         self.addrs[i],
                                                         self.randbinfile,
